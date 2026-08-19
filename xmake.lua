adcf89699b16fbe66d82b08fc7c54c6beb8769ea
Reda <redaesaam12334@gmail.com>
2026-08-19 03:33:01 +0300

    Force update xmake configuration

--- xmake.lua
+++ xmake.lua
@@ -1,11 +1,9 @@
 add_rules("mode.debug", "mode.release")
-
 add_requires("amethystapi")
 
 target("Better-Inventory")
     set_kind("shared")
     set_languages("c++20")
-    
     add_deps("amethystapi")
     add_files("src/**.cpp")
     add_includedirs("src")
